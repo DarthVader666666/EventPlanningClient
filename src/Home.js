cee40420a920d0928b5880987fdc7e0d8ddbd275
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-10-26 22:16:33 +0300

    Commented url const

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import useFetch from "./useFetch";
 
 const Home = () => {
   // const environmentName = process.env.REACT_APP_ENV;
-  const serverBaseUrl = process.env.REACT_APP_API_URL;
+  //const serverBaseUrl = process.env.REACT_APP_API_URL;
   const { error, isPending, data: events } = useFetch(`event-planning-server.azurewebsites.net/events/`)
 
   return (
