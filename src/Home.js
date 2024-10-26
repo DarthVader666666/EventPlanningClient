367184b3246766f7146be133299c58893450ee9a
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-10-26 22:13:44 +0300

    Used hardcoded request url

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import useFetch from "./useFetch";
 const Home = () => {
   // const environmentName = process.env.REACT_APP_ENV;
   const serverBaseUrl = process.env.REACT_APP_API_URL;
-  const { error, isPending, data: events } = useFetch(`${serverBaseUrl}/events/`)
+  const { error, isPending, data: events } = useFetch(`event-planning-server.azurewebsites.net/events/`)
 
   return (
     <div className="home">
