19160170c8ff80dc4c91fc9832cb8069fb1ba2a7
V.Rumyantsev <V.Rumyantsev@48-1335>
2024-10-28 16:45:07 +0300

    Refactored url

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,8 @@ import useFetch from "./useFetch";
 
 const Home = () => {
   // const environmentName = process.env.REACT_APP_ENV;
-  const serverBaseUrl = process.env.REACT_APP_API_URL;
-  const { error, isPending, data: events } = useFetch(`${serverBaseUrl}/events/`)
+  const url = process.env.REACT_APP_API_URL + '/events/';
+  const { error, isPending, data: events } = useFetch(url)
 
   return (
     <div className="home">
